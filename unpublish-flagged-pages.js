41e4f972d4013780d7250c29a1b260a3264472ab
Matthew Blissett <mblissett@gbif.org>
2024-01-30 14:46:21 +0100

    Exclude pages from production.

---
+++ unpublish-flagged-pages.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports.register = function () {
+  this.on('documentsConverted', ({ contentCatalog }) => {
+    contentCatalog.getPages((page) => page.out).forEach((page) => {
+      if (page.asciidoc?.attributes['page-unpublish'] != null) {
+        delete page.out
+      }
+    })
+  })
+}
